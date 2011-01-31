aca0262e38dafd5a8631631eecdb17ba7d57b2f1
mattswe <mattswe@gmail.com>
2011-01-30 21:22:43 -0800

    fixed odd number lengths

--- level1.py
+++ level1.py
@@ -2,9 +2,12 @@ textblock = 'Fourscoreandsevenyearsagoourfaathersbroughtforthonthiscontainentane
 
 def verify_palindrome(str):
 	if str[::-1] == str:
-		print len(str),str
+		if len(str) > 1:
+			print len(str),str
 
 for index,char in enumerate(textblock):
 	distance = min(len(textblock)-index,index)
 	for length in range(1,distance):
 		verify_palindrome(textblock[index-length:index+length])
+		verify_palindrome(textblock[index-length+1:index+length])
+		verify_palindrome(textblock[index-length:index+length+1])
