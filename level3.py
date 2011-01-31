cc14f3636b1f89da697ad377ef18ddd5dbf63b19
mattswe <mattswe@gmail.com>
2011-01-30 23:20:23 -0800

    done

--- level3.py
+++ level3.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 numbers = [3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99]
 
 def valid_subset(list_nums):
@@ -7,6 +9,11 @@ def valid_subset(list_nums):
 	return False
 
 
-for index,n in enumerate(numbers):
-	for size in range(index+1,len(numbers)):
-		print numbers[index:size]
+count = 0
+for size in range(2,len(numbers)):
+	for subset in combinations(numbers,size):
+		if valid_subset(list(subset)):
+			print subset
+			count = count + 1
+
+print 'count= ',count
